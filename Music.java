ceb59cef668a4e47d58112e3f07591af9345db85
Bill <iamkg05@gmail.com>
2016-03-05 20:03:03 -0600

    modified documents

--- Music.java
+++ Music.java
@@ -7,8 +7,8 @@ public class Music{
 	    String musicFile = null;
 	    InputStream in = null;
 	    AudioStream audioStream = null;
-
-	    System.out.println("1.sunday  2.win1(3rd)  3.win2(movie)  4.win3(1st)  5.lose1(4th)  6.lose2(3rd)  7.lose3(2nd)  8.lose4(1st)  9.cheer  10.New Student  0.stop  99.exit");
+	    String soundOptions = "1.tada  2.win1(3rd)  3.win2(movie)  4.win3(1st)  5.lose1(4th)  6.lose2(3rd)  7.lose3(2nd)  8.bikehorn 9.lose4(1st)  10.cheer  11.applause 12.banana  0.stop  99.exit";
+	    System.out.println(soundOptions);
 
 	    Scanner s = new Scanner(System.in);
 		int option = s.nextInt();
@@ -16,43 +16,51 @@ public class Music{
 	    while(option != 99){
 	    	switch(option){
 	    	case 1:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/sunday.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/tada.wav";
 	    		break;
 
 	    	case 2:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/win1.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/win1.wav";
 	    		break;
 
 	    	case 3:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/win2.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/win2.wav";
 	    		break;		
 
 	    	case 4:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/win3.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/win3.wav";
 	    		break;
 
 	    	case 5:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/lose1.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/lose1.wav";
 	    		break;
 
 	    	case 6:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/lose2.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/lose2.wav";
 	    		break;
 
 	    	case 7:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/lose3.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/lose3.wav";
 	    		break;
 
 	    	case 8:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/lose4.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/bikehorn.wav";
 	    		break;
 
 	    	case 9:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/cheer.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/lose4.wav";
 	    		break;
 
 	    	case 10:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/newstudent.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/cheer.wav";
+	    		break;
+
+	    	case 11:
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/applause.wav";
+	    		break;
+
+	    	case 12:
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/banana.wav";
 	    		break;
 
 	    	case 0:
@@ -60,7 +68,7 @@ public class Music{
 	    		break;
 
 	    	default:
-	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA Music Effect/music/win1.wav";
+	    		musicFile = "/Users/Bill_Cheng/Desktop/TSA/music/win1.wav";
 	    		break;
 	    	}
 	    	if(option != 0){
@@ -69,7 +77,7 @@ public class Music{
 	    		AudioPlayer.player.start(audioStream);
  			}
 
-	    	System.out.println("1.sunday  2.win1(3rd)  3.win2(movie)  4.win3(1st)  5.lose1(4th)  6.lose2(3rd)  7.lose3(2nd)  8.lose4(1st)  9.cheer  10.New Student  0.stop  99.exit");
+	    	System.out.println(soundOptions);
 	    	option = s.nextInt();
 	    	if(option != 0)
 	    		AudioPlayer.player.stop(audioStream); 
